fca33857d02aa223938efed22945048c2f1e5b34
Brayder Berroteran <brayderb@gmail.com>
2019-01-30 11:53:10 -0300

    se creo destroy

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'todos/:id', to: 'todos#show', as:'todo'
   get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
   patch 'todos/:id', to: 'todos#update', as: 'update_todo'
+  delete 'todos/:id', to: 'todos#destroy', as: 'delete_todo'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'todos#index'
